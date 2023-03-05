b4942169e5a0005b1afc11a160027bd5d8301d5d
Prateek <prateekmohanty63@gmail.com>
2023-03-05 21:48:42 +0530

    Feat:Added opearation model

--- researchLab/Lab/models.py
+++ researchLab/Lab/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
+
+
+class operation(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    operation_type=models.CharField(max_length=100)
+    pick_beaker=models.CharField(max_length=100)
+    reactants=models.JSONField()
+
 
 
 class beaker(models.Model):
